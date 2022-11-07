7c5193927c0bb473221aaba917ee2ac4057d71e4
mosraful21 <mosraful21@gmail.com>
2022-11-08 01:51:38 +0600

    tour places data added

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
+    return (
+        <div>
+            <div className='text-center mb-4'>
+                <p className="text-2xl font-bold text-orange-600">Services</p>
+                <h2 className="text-5xl font-semibold m-3">Our Service Area</h2>
+                <p className='w-3/5 m-auto'>Habib</p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-5'>
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
+                    ></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
